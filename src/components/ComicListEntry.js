90b9126f59010b6533edcf518d1cec680f8eb277
Zander Work <zander@zanderwork.com>
2020-03-18 09:51:35 -0700

    added grid of photos instead of list for main comics

--- src/components/ComicListEntry.js
+++ src/components/ComicListEntry.js
@@ -1,11 +1,33 @@
+/** @jsx jsx */
+
 import React from 'react';
 
+import { jsx } from '@emotion/core';
+import styled from '@emotion/styled/macro';
+
+const Content = styled.div`
+    display: block;
+    text-align: center;
+    margin: 50px;
+`;
+
+const Title = styled.span`
+    display: inline-block;
+`;
+
 function ComicListEntry(props) {
+    // return (
+    //     <div>
+    //         <a href={`/comics/${props.comic.id}`}>{props.comic.title}</a>
+    //     </div>
+    // );
     return (
-        <div>
-            <a href={`/comics/${props.comic.id}`}>{props.comic.title}</a>
-        </div>
-    );
+        <Content>
+            <a href={`/comics/${props.comic.id}`}><img height="400" src={props.comic.photoUrl} alt="comic photo" /></a>
+            <br />
+            <Title>{props.comic.title}</Title>
+        </Content>
+    )
 }
 
 export default ComicListEntry;
