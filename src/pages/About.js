e195090486ea2438984a9f57be457e2648c2d810
christa <christa@christas-MacBook-Pro.local>
2020-03-18 00:07:18 -0700

    Adds styling to Home and About pages

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function About() {
+
+
+    return (   
+        <div>
+            <h1>About</h1>
+            <p>Until recently, the prevailing view assumed lorem ipsum was born as a nonsense text. “It's not Latin, though it looks like it, and it actually says nothing,” Before & After magazine answered a curious reader, “Its ‘words’ loosely approximate the frequency with which letters occur in English, which is why at a glance it looks pretty real.”
+            <br/>
+            <br/>
+            As Cicero would put it, “Um, not so fast.”
+            <br/>
+            <br/>
+            The placeholder text, beginning with the line “Lorem ipsum dolor sit amet, consectetur adipiscing elit”, looks like Latin because in its youth, centuries ago, it was Latin.
+            <br/>
+            <br/>
+            Richard McClintock, a Latin scholar from Hampden-Sydney College, is credited with discovering the source behind the ubiquitous filler text. In seeing a sample of lorem ipsum, his interest was piqued by consectetur—a genuine, albeit rare, Latin word. Consulting a Latin dictionary led McClintock to a passage from De Finibus Bonorum et Malorum (“On the Extremes of Good and Evil”), a first-century B.C. text from the Roman philosopher Cicero. </p>
+        </div>
+    );
+}
+
+export default About;
