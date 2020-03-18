35096aa00e6402826e181b4f3c305e630f067bda
christa <christa@christas-MacBook-Pro.local>
2020-03-17 23:06:49 -0700

    Actually adds navbar?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import styled from '@emotion/styled/macro';
+import { Switch, Route } from 'react-router-dom';
+
+
+import Navbar from './components/Navbar';
+
 
 function App() {
     return (
         <div>
+            <Navbar />
             Hello World!
         </div>
     );
